1a1a8936bb4e2eb824373fdd870cd93e184d4fed
merigo <merigo@EdTaghan>
2020-03-09 21:36:17 -0400

    issues wrong starting packaging for msscbeerservice

--- src/main/java/edtaghan/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/edtaghan/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -1,4 +1,4 @@
-package edtaghan.springframework;
+package edtaghan.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
